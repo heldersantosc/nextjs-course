b1cd37bc8aa6659f0fdebece81820b5a9f9edc7c
Helder Santos <heldersantos@bemol.com.br>
2022-10-27 11:26:28 -0400

    alternativas para montar um Link
    Signed-off-by: Helder Santos <heldersantos@bemol.com.br>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,11 @@
 import Link from "next/link";
 
 export default function Home() {
+  const clients = [
+    { id: 1, value: "helder" },
+    { id: 2, value: "santos" },
+  ];
+
   return (
     <div>
       <ul>
@@ -13,6 +18,23 @@ export default function Home() {
         <li>
           <Link href={"/blog/22"}>blog 22</Link>
         </li>
+        {clients.map((client) => {
+          return (
+            <li key={client.id}>
+              <Link
+                key={client.id}
+                href={{
+                  pathname: "clients/[id]",
+                  query: {
+                    id: client.id,
+                  },
+                }}
+              >
+                {client.value}
+              </Link>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
