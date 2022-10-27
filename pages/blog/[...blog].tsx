39f9950d8382891d6c831f763405041d8d09e3a4
Helder Santos <heldersantos@bemol.com.br>
2022-10-27 10:59:14 -0400

    pegando todos as queries do path
    Signed-off-by: Helder Santos <heldersantos@bemol.com.br>

--- pages/blog/[...blog].tsx
+++ pages/blog/[...blog].tsx
@@ -1,6 +1,6 @@
 import { useRouter } from "next/router";
 
-export default function PortifolioId() {
+export default function Blogs() {
   const router = useRouter();
 
   return <div>{JSON.stringify(router.query)}</div>;
